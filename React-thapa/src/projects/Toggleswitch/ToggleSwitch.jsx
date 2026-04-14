5f98a8bdbdc9957a97394038548c934a52fc92bb
Yashgiri <yashgoswami3251@gmail.com>
2026-04-14 09:18:26 +0530

    import react icons

--- React-thapa/src/projects/Toggleswitch/ToggleSwitch.jsx
+++ React-thapa/src/projects/Toggleswitch/ToggleSwitch.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import "./ToggleSwitch.css";
 
+// Here we are import { IoIosSwitch } this icons from react icons library 
+
+import { IoIosSwitch } from "react-icons/io";
+
+
 export const ToggleSwitch = () => {
     const [isOn, setIsOn] = useState(false);
 
@@ -12,11 +17,14 @@ export const ToggleSwitch = () => {
     const toggleBGColor = { backgroundColor: isOn ? "#4caf50" : "" };
 
     return (
-        <div className="toggle-switch" style={toggleBGColor}
-            onClick={handleToggleSwitch}>
-            <div className={`switch ${checkIsOn}`}>
-                <span className="switch-state">{checkIsOn}</span>
+        <>
+            <h1> Toggle Siwtch <IoIosSwitch style={{ color: "yellow" }} /></h1>
+            
+            <div className="toggle-switch" style={toggleBGColor} onClick={handleToggleSwitch}>
+                <div className={`switch ${checkIsOn}`}>
+                    <span className="switch-state">{checkIsOn}</span>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
