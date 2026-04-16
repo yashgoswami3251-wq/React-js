2721a16f0ee628e5672c55a0aeaffddd2fedd92c
Yashgiri <yashgoswami3251@gmail.com>
2026-04-16 23:20:50 +0530

    Set Date and time in todo application

--- React-thapa/src/projects/Toggleswitch/Todo/Todo.jsx
+++ React-thapa/src/projects/Toggleswitch/Todo/Todo.jsx
@@ -1,24 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Todo.css";
 import { MdCheck } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
 
 export const Todo = () => {
-    const[inputvalue, setInputvalue] = useState("");
+    const [inputvalue, setInputvalue] = useState("");
     const [task, setTask] = useState([]);
 
-    const handleInputchange = (value) =>{
+    // This is date and time state use this is bcz this is updated every second
+    const [datetime, setDateTime] = useState("");
+
+    const handleInputchange = (value) => {
         setInputvalue(value);
     }
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
 
-        if(!inputvalue) return;
+        if (!inputvalue) return;
 
-        if(task.includes(inputvalue)) {
+        if (task.includes(inputvalue)) {
             setInputvalue("");
-             return;
+            return;
         }
 
         setTask((prevTask) => [...prevTask, inputvalue]);
@@ -26,11 +29,35 @@ export const Todo = () => {
         setInputvalue("");
     };
 
+    // todo date and time
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const currentDate = now.toLocaleDateString();
+            const currentTime = now.toLocaleTimeString();
+
+            // used state 
+            setDateTime(`${currentDate} - ${currentTime}`);
+        }, 500);
+        return() => clearInterval(interval);
+    })
+
+    const interval = setInterval(() => {
+        const now = new Date();
+        const currentDate = now.toLocaleDateString();
+        const currentTime = now.toLocaleTimeString();
+
+        setDateTime(`${currentDate} - ${currentTime}`);
+
+    }, 500);
+
     return (
         <section className="todo-container">
             <header>
                 <h1>Todo List</h1>
+                <h2 className="date-time">{datetime}</h2>
             </header>
+
             <section className="form">
                 <form onSubmit={handleFormSubmit}>
                     <div>
@@ -43,7 +70,7 @@ export const Todo = () => {
             </section>
 
             <section className="myUnOrdList">
-                 <ul>
+                <ul>
                     {
                         task.map((curTask, index) => {
                             return <li key={index} className="todo-item">
@@ -57,8 +84,9 @@ export const Todo = () => {
                             </li>
                         })
                     }
-                 </ul>
+                </ul>
             </section>
+
         </section>
     );
 };
